98dbf365e1ab38cecc6fdab5c6bf4ada307ab387
Joshua Hoblitt <josh@hoblitt.com>
2016-01-26 15:32:51 -0700

    declare dwarf-2 in cflags
    To resolve this build error observed when building on el5 with the conda
    gcc 4.8.5 package:
    
        /usr/bin/ld: Dwarf Error: found dwarf version '4', this reader only
        handles version 2 information.
        ../../mysys_ssl/libmysys_ssl.a(my_crypt.cc.o): In function
        `TaoCrypt::Mode_BASE::~Mode_BASE()':
        my_crypt.cc:(.text._ZN8TaoCrypt9Mode_BASED0Ev[_ZN8TaoCrypt9Mode_BASED0Ev]+0xf):
        undefined reference to `operator delete(void*)'

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -3,5 +3,7 @@
 config()
 {
     # TokuDB requires third-party software jemalloci (see http://www.canonware.com/jemalloc/)
-    cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX} -DPLUGIN_TOKUDB=NO
+    cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX} -DPLUGIN_TOKUDB=NO \
+        -DCMAKE_C_FLAGS_RELWITHDEBINFO="-g -gdwarf-2" \
+        -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-g -gdwarf-2"
 }
