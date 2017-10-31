8a54c4644ee4f063aa4d2a9fd8c3f7b56a52afed
Fritz Mueller <fritzm@slac.stanford.edu>
2017-10-30 17:59:44 -0700

    Remove patches/code for EL5 support

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -24,13 +24,6 @@ config()
 
     case $(uname) in
         Linux*)
-            if grep -q -i "CentOS release 5" /etc/redhat-release; then
-                # mroonga does not build on EL5 (conda ref platform) and is
-                # unused by LSST
-                ARGS+=('-DPLUGIN_MROONGA=NO')
-                # required to build on EL5
-                ARGS+=('-DHAVE_CXX_NEW=NO')
-            fi
             ;;
         Darwin*)
             # Disable external SSL on OS X as it is not a standard library
