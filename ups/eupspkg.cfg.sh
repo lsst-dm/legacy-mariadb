1d117e32b0e3645077fcb03cb641291626892bbf
fritzm@slac.stanford.edu <fritzm@slac.stanford.edu>
2018-04-05 18:38:56 -0700

    Adjust config for SSL, ZLIB, PCRE
    Make eupspkg.cfg.sh consisted with the one in mariadbclient to the
    greatest extent possible to avoid inadvertent inconsistent options
    between these two packages.
    
    New configuration uses bundled ZLIB and PCRE, and system SSL. SSL
    is no longer optional for MariaDB; MacOS users may, for example,
    "brew install gnutls" to satisfy this dependency.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -13,28 +13,17 @@ config()
     # TokuDB requires third-party software jemalloci (see http://www.canonware.com/jemalloc/)
     ARGS+=('-DPLUGIN_TOKUDB=NO')
 
-    # Always use bundled PCRE
+    # Due to cmake library discovery being overly energetic (searching every
+    # lib directory relative to each entry in $PATH) we use the bundled ZLIB and
+    # PCRE libraries to avoid link confusion downstream
+    ARGS+=('-DWITH_ZLIB=bundled')
     ARGS+=('-DWITH_PCRE=bundled')
 
     # Prevent CMake from finding and linking against libraries distributed in $(dirname
-    # python)/../lib. This is CMake's default behaviour, but can cause us to erroneously link
+    # python)/../lib. This is CMake's default behavior, but can cause us to erroneously link
     # against libraries distributed by e.g. Anaconda.
     PYTHONLIBDIR=$(which python | sed -e's|bin/python|lib|')
     ARGS+=("-DCMAKE_SYSTEM_IGNORE_PATH=${PYTHONLIBDIR}")
-
-    case $(uname) in
-        Linux*)
-            ;;
-        Darwin*)
-            # Disable external SSL on OS X as it is not a standard library
-            # and LSST does not need full SSL support.
-            ARGS+=('-DWITH_SSL=bundled')
-            ;;
-        *)
-            # non-fatal
-            echo "unsupported platform: $(uname)"
-            ;;
-    esac
 
     cmake . "${ARGS[@]}"
 }
