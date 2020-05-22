035589a699974209447fe5d0373beb174d4e0f36
Brian Van Klaveren <bvan@slac.stanford.edu>
2020-05-22 11:53:10 -0700

    Disable dtrace and use bundled libs

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -13,15 +13,18 @@ config()
     # Remove mysql-test directory (~330 MB) to reduce installation size
     ARGS+=("-DINSTALL_MYSQLTESTDIR=")
 
+    ARGS+=("-DENABLE_DTRACE=NO")
+
     # TokuDB requires third-party software jemalloci (see http://www.canonware.com/jemalloc/)
     ARGS+=('-DPLUGIN_TOKUDB=NO')
     ARGS+=('-DPLUGIN_CASSANDRA=NO')
+    ARGS+=('-DPLUGIN_ROCKSDB=NO')
 
     # Due to cmake library discovery being overly energetic (searching every
     # lib directory relative to each entry in $PATH) we use the bundled ZLIB and
     # PCRE libraries to avoid link confusion downstream
-    ARGS+=('-DWITH_ZLIB=bundled')
-    ARGS+=('-DWITH_PCRE=bundled')
+    # ARGS+=('-DWITH_ZLIB=bundled')
+    # ARGS+=('-DWITH_PCRE=bundled')
 
     # Prevent CMake from finding and linking against libraries distributed in $(dirname
     # python)/../lib. This is CMake's default behavior, but can cause us to erroneously link
