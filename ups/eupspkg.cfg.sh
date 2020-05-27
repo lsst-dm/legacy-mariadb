a4b62d995ba7c9fb6d3cd70f1d43dd4bdc63d3d3
Kian-Tat Lim <ktl@slac.stanford.edu>
2020-05-27 00:25:08 +0000

    Allow use of conda libraries.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -7,9 +7,6 @@ config()
 
     ARGS+=("-DCMAKE_INSTALL_PREFIX=${PREFIX}")
 
-    # Don't pick up e.g. conda boost by accident
-    ARGS+=("-DBoost_NO_BOOST_CMAKE=ON")
-
     # Remove mysql-test directory (~330 MB) to reduce installation size
     ARGS+=("-DINSTALL_MYSQLTESTDIR=")
 
@@ -19,18 +16,6 @@ config()
     ARGS+=('-DPLUGIN_TOKUDB=NO')
     ARGS+=('-DPLUGIN_CASSANDRA=NO')
     ARGS+=('-DPLUGIN_ROCKSDB=NO')
-
-    # Due to cmake library discovery being overly energetic (searching every
-    # lib directory relative to each entry in $PATH) we use the bundled ZLIB and
-    # PCRE libraries to avoid link confusion downstream
-    # ARGS+=('-DWITH_ZLIB=bundled')
-    # ARGS+=('-DWITH_PCRE=bundled')
-
-    # Prevent CMake from finding and linking against libraries distributed in $(dirname
-    # python)/../lib. This is CMake's default behavior, but can cause us to erroneously link
-    # against libraries distributed by e.g. Anaconda.
-    # PYTHONLIBDIR=$(command -v python | sed -e's|bin/python|lib|')
-    # ARGS+=("-DCMAKE_SYSTEM_IGNORE_PATH=${PYTHONLIBDIR}")
 
     cmake . "${ARGS[@]}"
 }
